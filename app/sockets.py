250eb398765adab3507d37840199cce0e341e260
margusengso <margusengso@hotmail.com>
2024-11-21 10:02:23 +0200

    refactor

--- app/sockets.py
+++ app/sockets.py
@@ -1,32 +1,30 @@
 import requests
 from flask_socketio import emit
+from os import getenv
 
 def message_received(msg):
-    print(f"Message received: {msg}")
-
     if len(msg) < 1:
         emit('response', {'status': 'error', 'message': "empty string received"})
         return
 
-        # Send the message to the n8n webhook
-    n8n_url = "https://margusengso.app.n8n.cloud/webhook/703b38d1-2ba1-45ad-86d2-458031dc1e4f"
+
+    n8n_url = "https://margusengso.app.n8n.cloud/webhook/" + str(getenv('N8N_HOOK'))
     headers = {
         "Content-Type": "application/json"
     }
     try:
-        # Make the POST request to n8n with headers
         response = requests.post(n8n_url, json={"the_text": msg}, headers=headers, timeout=10)
         if response.status_code == 200:
             data = response.json()
 
             # Since the response is a list, access the first element
             if isinstance(data, list) and len(data) > 0:
-                file_info = data[0]  # Get the first item from the list
+                file_info = data[0]
                 file_id = file_info.get("id")
                 if file_id:
                     proxy_url = f"https://pythonprojectn8n.onrender.com/proxy-audio?file_id={file_id}"
                     reply = {'status': 'success', 'url': proxy_url}
-                    emit('response', reply)  # Emit the structured response to the client
+                    emit('response', reply)
                 else:
                     emit('response', {'status': 'error', 'message': "No file ID found in the response."})
             else:
--- application.py
+++ application.py
@@ -5,29 +5,21 @@ import os
 from flask import Flask
 from flask_socketio import SocketIO
 from dotenv import load_dotenv
-# from app.tasks import job1
 from app.sockets import message_received
 from app.routes import init_routes
-# import schedule
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('WS_SECRET_KEY')
 
-
-# Initialize SocketIO with gevent
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent")
 
-# schedule.every(1).minutes.do(job1)
-
 init_routes(app)
-
 
 @socketio.on('message')
 def handle_message(msg):
     message_received(msg)
-
 
 
 if __name__ == '__main__':
